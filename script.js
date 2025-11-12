0bec89b8e241d9390af6d9e1f577d5a5724c8b95
Allan Pires <243133092+alltecgraf@users.noreply.github.com>
2025-11-12 16:54:09 -0300

    working calculator handle only one number at a time

--- script.js
+++ script.js
@@ -7,19 +7,83 @@ function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
 
 function operate(num1, operator, num2) {
+    let result = 0;
+
     switch (operator) {
         case '+':
-            add(num1, num2);
+            result = add(num1, num2);
             break;
         case '-':
-            subtract(num1, num2);
+            result = subtract(num1, num2);
             break;
         case '*':
-            multiply(num1, num2);
+            result = multiply(num1, num2);
             break;
         case '/':
-            divide(num1, num2);
+            result = divide(num1, num2);
             break;
         default: alert("OPERATOR ERROR");
     }
+
+    visorDisplay.textContent = result;
+    return result;
 }
+
+function handleNumberInput(event) {
+    // Only update display to empty after the input of a new number
+    if (!currentNum) {
+        visorDisplay.textContent = '';
+    }
+
+    let input = event.target.textContent;
+    currentNum += input;
+    visorDisplay.textContent = currentNum;
+}
+
+function handleOperationInput(event) {
+    // if i already have an operator, do calculation
+    // else, store operator for use after the input of second number 
+    if (operator) {
+        prevNum = operate(prevNum, operator, parseInt(currentNum));
+        currentNum = resetNum();
+    } else {
+        prevNum = parseInt(currentNum);
+        currentNum = resetNum();
+    }
+
+    // if operator is of class calculate (e.g. "="), set operator to null
+    if (event.target.classList.contains("button-calculate")) {
+        operator = null;
+        currentNum = prevNum;
+        prevNum = resetNum()
+    } else {
+        operator = event.target.textContent;
+    }
+}
+
+function clear(event) {
+    visorDisplay.textContent = '';
+    prevNum;
+    currentNum = resetNum();
+}
+
+function resetNum() {return '';}
+
+const visorDisplay = document.querySelector('#visor');
+const input = document.querySelectorAll('button');
+
+let prevNum;
+let operator;
+let currentNum = resetNum();
+
+input.forEach(element => {
+    if (element.getAttribute("class") === "button-reset") {
+        element.addEventListener('click', clear);
+    }
+    else if (element.getAttribute("class") === "button-number") {
+        element.addEventListener('click', handleNumberInput)
+    }
+    else if (element.classList.contains("button-operation")) {
+        element.addEventListener('click', handleOperationInput);
+    } 
+});
